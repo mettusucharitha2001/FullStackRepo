abb4d65fc5ee21be909a48e62479ed93841a5598
suchitra mettu <suchitra mettu@LAPTOP-1FHRGL8T>
2024-02-27 12:36:29 +0530

    task2

---
+++ Proj01/src/main/java/com/nt/main/AppTest.java
@@ -0,0 +1,18 @@
+package com.nt.main;
+
+import com.nt.service.ArithmeticOperations;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest 
+{
+    public static void main( String[] args )
+    {
+       ArithmeticOperations ar=new ArithmeticOperations();
+        System.out.println("sum:"+(ar.sum(20,10)));   
+        System.out.println("sub:"+(ar.sub(20,10))); 
+        System.out.println("mul:"+(ar.mul(2,1)));
+    }
+}
