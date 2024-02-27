ef05cc135cbd56c0a12b880c15936630b9326074
suchitra mettu <suchitra mettu@LAPTOP-1FHRGL8T>
2024-02-27 12:48:02 +0530

    task6

--- Proj01/src/main/java/com/nt/main/AppTest.java
+++ Proj01/src/main/java/com/nt/main/AppTest.java
@@ -13,6 +13,6 @@ public class AppTest
        ArithmeticOperations ar=new ArithmeticOperations();
         System.out.println("sum:"+(ar.sum(20,10)));   
         System.out.println("sub:"+(ar.sub(20,10)));//123 456
-        System.out.println("mul:"+(ar.mul(2,1)));//123456
+        System.out.println("mul:"+(ar.mul(2,1)));//901
     }
 }
