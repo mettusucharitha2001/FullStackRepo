bc53d227ba46b868405bcbd6883f85d96231dd6d
mettusucharitha2001 <mettusucharitha2001@gmail.com>
2024-02-28 14:04:22 +0530

    branch

--- Proj01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ Proj01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -1,5 +1,10 @@
 package com.nt.payment;
 
 public class PaymentOperationsService {
-
+	public String doCreditCardPayment() {
+		return "credit card payment is done";
+	}
+	public String doDebitCardPayment() {
+		return "debit card payment is done";
+	}
 }
